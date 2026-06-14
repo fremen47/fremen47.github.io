697c52c1823d7471241876e38498389b5bcc1e85
Frederik Remen <frederik.remen4@gmail.com>
2026-06-14 13:15:20 +0200

    Hamburger menu fixes

--- README.md
+++
@@ -1,7 +0,0 @@
-# I used AI to help generate czech text (because I come from Slovakia), I also used it to help me fix problems that I encountered while creating the web such as some responsivity functions, css classes i did not know (white-space: nowrap...).
-
-# I also focused to make every javascript by myself, that is why it is so simple, but I used AI to help me close the hamburger menu after clicking on element and to close wrapper by clicking escape
-
-# HTML structure, classes and all other things i tried to do only by myself
-
-# What i did not have time to do is contact form but i did not want to use AI to do it for me
--- hamburger.js
+++ hamburger.js
@@ -3,14 +3,20 @@ const menu = document.querySelector(".hamburger_menu");
 const hamburger = document.querySelector(".hamburger");
 const close = document.querySelector(".close_hamb");
 const open = document.querySelector(".open_hamb");
+const header = document.getElementById('header');
+const main = document.querySelector("main");
 
 function toggleMenu() {
   if (menu.classList.contains("show_menu")) {
     menu.classList.remove("show_menu");
+    header.classList.remove("scrolled");
+    main.style.opacity = "1";
     close.style.display = "none";
     open.style.display = "block";
   } else {
     menu.classList.add("show_menu");
+    header.classList.add("scrolled");
+    main.style.opacity = "0.3";
     close.style.display = "block";
     open.style.display = "none";
   }
